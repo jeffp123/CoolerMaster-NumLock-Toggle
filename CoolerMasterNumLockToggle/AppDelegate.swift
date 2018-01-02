a491b4295af561228844fabc9b68b2747d621776
jeffpeck <chaimpeck@gmail.com>
2018-01-02 18:03:47 -0500

    Initial Commit

---
+++ CoolerMasterNumLockToggle/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  CoolerMasterNumLockToggle
+//
+//  Created by Jeff Peck - Home on 1/2/18.
+//  Copyright © 2018 Jeff Peck. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
